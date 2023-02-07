e8f4bb735d4c92bcc52104fa1eb7186139cea3af
mewpotchanad <natthapotchanad@gmail.com>
2023-02-07 17:06:06 +0700

    middleware

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-const {sequelize} = require('./models')
-sequelize.sync({force:true})
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const chalk = require('chalk');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+
+const app = express()
+
+const notFoundMiddleware = require('./middlewares/not-found')
+const errorMiddleware = require('./middlewares/error')
+
+app.use(morgan('dev'))
+app.use(helmet());
+app.use(cors())
+app.use(express.json())
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+app.listen()
+
+const port = process.env.PORT || 8000;
+app.listen(port, () =>
+  console.log(chalk.yellowBright.italic.bold(`server running on port: ${port}`))
+);
