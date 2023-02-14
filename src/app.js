fd69fc86bafa3462b5a0b837137c99f841143f84
mewpotchanad <natthapotchanad@gmail.com>
2023-02-14 10:14:12 +0700

    edit info

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const rateLimit = require('express-rate-limit');
 const app = express()
 
 const authRoute = require('./routes/auth-route')
+const userRoute = require('./routes/user-route')
 const notFoundMiddleware = require('./middlewares/not-found')
 const errorMiddleware = require('./middlewares/error')
 
@@ -32,7 +33,9 @@ app.use(helmet());
 app.use(cors())
 app.use(express.json())
 
+
 app.use('/auth', authRoute)
+app.use('/user', userRoute)
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
---
+++ src/controllers/user-controller.js
@@ -0,0 +1,12 @@
+const { User } = require('../models');
+
+exports.editProfile = async (req, res, next) => {
+    try {
+        const value = req.body
+
+        await User.update(value, { where: { id: req.user.id } });
+        res.status(200).json(value);
+    } catch (err) {
+        next(err)
+    }
+}
